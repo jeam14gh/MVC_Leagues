f5baee928467160e8661d1ee5865b16f554324f1
Jhon Esneider <jeammsn@hotmail.com>
2019-11-05 23:38:50 -0500

    Cargar fechas, actualizarlas y guardar en BD. Ligas actualizadas

--- MVC_League/Models/Data/MatchData.cs
+++ MVC_League/Models/Data/MatchData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using MVC_League.Entities;
+using System.Data.Entity.Migrations;
 
 namespace MVC_League.Models.Data
 {
@@ -16,8 +17,21 @@ namespace MVC_League.Models.Data
 
         public void AddMany(List<Match> matches)
         {
-            db.Matches.AddRange(matches);
-            db.SaveChanges();
+            //db.Matches.AddRange(matches);
+            //db.SaveChanges();
+
+            try
+            {
+                foreach (var match in matches)
+                {
+                    db.Matches.AddOrUpdate(u => u.Id, match);
+                }
+                db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Error.Message = "Error al agregar o actualizar partidos: " + ex.Message;
+            }
         }
 
         // Obtiene todas las fechas por el numero de la fecha
